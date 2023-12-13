cdeb246d45721d03e3e65821baf7663cbf320df5
LiveWithCodeAnkit <ankityadav119live@gmail.com>
2023-12-14 00:29:12 +0530

    modify home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,14 @@
 import React from "react";
 import Layout from "./layout/Layout";
 import Home from "./dashbord";
+import AboutPage from "./about";
 
 export default function HomePage() {
   return (
     <>
       <Layout>
-        <Home />
+        <AboutPage/>
+        {/* <Home /> */}
       </Layout>
     </>
   );
